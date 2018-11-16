6ebf6b476bad626ccc07c1dd0c376e22039e19a0
Jorge Baron <jorge.baron@adaits.es>
2018-11-16 01:15:53 +0100

    Se incluyen ejemplos de CSS

--- Ejemplo_Trabajo/javascript/clases/clases.js
+++ Ejemplo_Trabajo/javascript/clases/clases.js
@@ -1,93 +1,99 @@
 class Pelicula{
-    constructor(nombre,director,categoria){
-        this.nombre = nombre;
-        this.director = director;
-        this.categoria = categoria;
-    }
+	constructor(nombre,director,categoria,img){
+		this.nombre = nombre;
+		this.director = director;
+		this.categoria = categoria;
+		this.img = img;
+	}
+	
+	get nombre(){
+		return this._nombre;
+	}
 
-    get nombre(){
-        return this._nombre;
-    }
+	set nombre(nombre){
+		this._nombre = nombre;
+	}
 
-    set nombre(nombre){
-        this._nombre = nombre;
-    }
+	get director(){
+		return this._director;
+	}
 
-    get director(){
-        return this._director;
-    }
-
-    set director(director){
-        this._director = director;
-    }
+	set director(director){
+		this._director = director;
+	}
     
-    get categoria(){
-        return this._categoria;
-    }
+	get categoria(){
+		return this._categoria;
+	}
 
-    set categoria(categoria){
-        this._categoria = categoria;
-    }
+	set categoria(categoria){
+		this._categoria = categoria;
+	}
 
-    mostrarEnHTML(nodoHTML){
-        let bloque = document.createElement("div");
-        bloque.className = "pelicula";
-        let titulo = document.createElement("h1");
-        titulo.innerHTML = this.nombre; 
-        let categoria = document.createElement("h2");
-        categoria.innerHTML = this.categoria;
-        let director = document.createElement("p");
-        director.innerHTML = this.director.nombre;
-        bloque.append(titulo);
-        bloque.append(categoria);
-        bloque.append(director);
-        nodoHTML.appendChild(bloque);
-    }
+	mostrarEnHTML(nodoHTML){
+		let bloque = document.createElement('div');
+		bloque.className = 'pelicula';
 
-    contieneNombre(nombre){
-        let contiene = false;
-        let nombreTratado = tratarCadenasInput(nombre);
-        if(nombreTratado !== null){
-            contiene = this.nombre.toUpperCase().includes(nombreTratado);
-        }
-        return contiene;
-    }
+		let imagen = document.createElement('img');
+		imagen.src = `imagenes/${this.img}`;
+		let titulo = document.createElement('h1');
+		titulo.innerHTML = this.nombre; 
+		let categoria = document.createElement('h2');
+		categoria.innerHTML = this.categoria;
+		let director = document.createElement('p');
+		director.innerHTML = this.director.nombre;
 
-    contieneDirector(director){
-        let contiene = false;
-        let directorTratado = tratarCadenasInput(director);
-        if(directorTratado != null){
-            contiene = this.director.nombre.toUpperCase().includes(directorTratado);
-        }
-        return contiene;
-    }
+		bloque.append(imagen);
+		bloque.append(titulo);
+		bloque.append(categoria);
+		bloque.append(director);
+		nodoHTML.appendChild(bloque);
+	}
+
+	contieneNombre(nombre){
+		let contiene = false;
+		let nombreTratado = tratarCadenasInput(nombre);
+		if(nombreTratado !== null){
+			contiene = this.nombre.toUpperCase().includes(nombreTratado);
+		}
+		return contiene;
+	}
+
+	contieneDirector(director){
+		let contiene = false;
+		let directorTratado = tratarCadenasInput(director);
+		if(directorTratado != null){
+			contiene = this.director.nombre.toUpperCase().includes(directorTratado);
+		}
+		return contiene;
+	}
 }
 
 class Director{
 
-    constructor(nombre){
-        this.nombre = nombre;
-        this.peliculas = [];
-    }
+	constructor(nombre){
+		this.nombre = nombre;
+		this.peliculas = [];
+	}
 
-    get nombre(){
-        return this._nombre;
-    }
+	get nombre(){
+		return this._nombre;
+	}
 
-    set nombre(nombre){
-        this._nombre = nombre;
-    }
+	set nombre(nombre){
+		this._nombre = nombre;
+	}
     
-    get peliculas(){
-        return this._peliculas;
-    }
+	get peliculas(){
+		return this._peliculas;
+	}
 
-    set peliculas(peliculas){
-        this._peliculas = peliculas;
-    }
+	set peliculas(peliculas){
+		this._peliculas = peliculas;
+	}
 
-    incluirPelicula(pelicula){
-        this._peliculas.push(pelicula);
-    }
+	incluirPelicula(pelicula){
+		this._peliculas.push(pelicula);
+	}
 
 }
--- Ejemplo_Trabajo/javascript/clases/objetos.js
+++ Ejemplo_Trabajo/javascript/clases/objetos.js
@@ -1,15 +1,15 @@
-let directorGL = new Director("George Lucas");
-let directorSS = new Director("Steven Spielbeg");
-let directorCN = new Director("Chirstopher Nolan");
-let directorJJA = new Director("JJ.Abraham");
+let directorGL = new Director('George Lucas');
+let directorSS = new Director('Steven Spielbeg');
+let directorCN = new Director('Chirstopher Nolan');
+let directorJJA = new Director('JJ.Abraham');
 
 let listaDirectores= [directorGL,directorSS,directorCN,directorJJA];
 
-let peliculas = []
+let peliculas = [];
 
-let peliculaSW = new Pelicula("Star Wars: El imperio Contrataca",directorGL,"Space Opera");
-let peliculaSW2 = new Pelicula("Star Wars: El retorno del Jedi",directorGL,"Space Opera");
-let peliculaSW3 = new Pelicula("Star Wars: Una nueva esperanza",directorGL,"Space Opera");
+let peliculaSW = new Pelicula('Star Wars: El imperio Contrataca',directorGL,'Space Opera','imperio_contrataca.jpg');
+let peliculaSW2 = new Pelicula('Star Wars: El retorno del Jedi',directorGL,'Space Opera','retorno_jedi.jpg');
+let peliculaSW3 = new Pelicula('Star Wars: Una nueva esperanza',directorGL,'Space Opera','nueva_esperanza.jpg');
 
 peliculas.push(peliculaSW);
 peliculas.push(peliculaSW2);
@@ -19,8 +19,8 @@ directorGL.incluirPelicula(peliculaSW);
 directorGL.incluirPelicula(peliculaSW2);
 directorGL.incluirPelicula(peliculaSW3);
 
-let peliculaJP = new Pelicula("Jurasic Park",directorSS,"Aventuras");
-let peliculaIJ = new Pelicula("Indiana Jone sy el arca perdida",directorSS,"Aventuras");
+let peliculaJP = new Pelicula('Jurasic Park',directorSS,'Aventuras','jurasicPark.jpg');
+let peliculaIJ = new Pelicula('Indiana Jones y el arca perdida',directorSS,'Aventuras','indiana_jones.jpg');
 
 peliculas.push(peliculaJP);
 peliculas.push(peliculaIJ);
@@ -30,9 +30,9 @@ directorSS.incluirPelicula(peliculaJP);
 directorSS.incluirPelicula(peliculaIJ);
 
 
-let peliculaO = new Pelicula("Origen",directorCN,"Ciencia Ficcion");
-let peliculaI = new Pelicula("Intellestellar",directorCN,"Ciencia Ficción");
-let peliculaB = new Pelicula("Batman: El caballero oscuro",directorCN,"Aventuras");
+let peliculaO = new Pelicula('Origen',directorCN,'Ciencia Ficcion','origen.jpg');
+let peliculaI = new Pelicula('Interstellar',directorCN,'Ciencia Ficción','interstellar.jpg');
+let peliculaB = new Pelicula('Batman: El caballero oscuro',directorCN,'Aventuras', 'batman.jpg');
 
 peliculas.push(peliculaO);
 peliculas.push(peliculaI);
@@ -42,8 +42,8 @@ directorCN.incluirPelicula(peliculaO);
 directorCN.incluirPelicula(peliculaI);
 directorCN.incluirPelicula(peliculaB);
 
-let peliculaSWD = new Pelicula("Star Wars: El despertar de la fuerza",directorJJA,"Space Opera");
-let peliculaST = new Pelicula("Stra Trek",directorJJA,"Ciencia Ficción");
+let peliculaSWD = new Pelicula('Star Wars: El despertar de la fuerza',directorJJA,'Space Opera','despertar.png');
+let peliculaST = new Pelicula('Star Trek',directorJJA,'Ciencia Ficción','StarTrek.jpg');
 
 peliculas.push(peliculaSWD);
 peliculas.push(peliculaST);
