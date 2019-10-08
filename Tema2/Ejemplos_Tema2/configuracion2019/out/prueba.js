75253e0563ce1d57fddb11721f5727bd3391cb59
Jorge <jorge.baron@adaits.es>
2019-10-08 21:29:22 +0200

    Configuracion 2019

---
+++ Tema2/Ejemplos_Tema2/configuracion2019/out/prueba.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @description Funcion de ejemplo.
+ * @param {nombre} nombre de prueba
+ * @return {valor} de prueba
+ */
+function prueba(nombre) {
+  var y = 'nuevo';
+  var x = nombre + ':codigo de ejemplo ' + y;
+  alert(x + y);
+  return 2;
+}
+
+/** @description Funcion de ejemplo.
+ * @param {x} x es sumando 1
+ * @param {y} y es sumando 2
+ * @return {valor} de suma
+ */
+function suma(x, y) {
+  return x + y;
+}
+
+/** @description Función de ejemplo.
+ * @param {nombre} nombre es el nombre de la persona
+ * @param {apellidos} apellidos son los apellidos de la persona
+ * @return {x} x es la concatenación del nombre con los apellidos.
+ */
+function prueba2(nombre, apellidos) {
+  var x = nombre + ' ' + apellidos;
+  return x;
+}
+
+console.log(prueba('Jorge'));
+console.log(suma(1, 2));
+console.log(prueba2('Jorge', 'Barón Abad'));
