6a9df87e7d60b43498980c27f8783ec23bf22541
Jorge <jorge.baron@adaits.es>
2019-11-17 19:27:39 +0100

    Ejercicios de repaso en clase

---
+++ Tema4/ejemplos_repaso/repaso.js
@@ -0,0 +1,12 @@
+let animal1 = {nombre:"Thor",especie:"Perro",raza:"caniche",vida:5};
+let animal2 = {nombre:"Capitan America",especie:"Gato",raza:"siames",vida:8};
+let animal3 = {nombre:"Viuda Negra",especie:"Perro",raza:"bulldog",vida:15};
+let animal4 = {nombre:"Hulk",especie:"Perro",raza:"pastor aleman",vida:12};
+let animal5 = {nombre:"Ojo de Halcon",especie:"Perro",raza:"san bernardo",vida:9};
+let animal6 = {nombre:"Loky",especie:"Perro",raza:"salchicha",vida:4};
+let animal7 = {nombre:"Iron Man",especie:"Conejo",raza:"pascuas",vida:9};
+let animal8 = {nombre:"WarMachine",especie:"Perro",raza:"raton de praga",vida:20};
+let animal9 = {nombre:"Vision",especie:"Perro",raza:"pastor belga",vida:12};
+let animal10 = {nombre:"Mercurio",especie:"Pajaro",raza:"loro",vida:15};
+
+let listadoAnimales = [animal1, animal2, animal3, animal4, animal5, animal6, animal7, animal8, animal9, animal10];
