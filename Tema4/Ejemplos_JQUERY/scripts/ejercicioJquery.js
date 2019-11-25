74a272dd76ec8cc970dd65d81dedc8272b67c83e
Jorge <jorge.baron@adaits.es>
2019-11-25 21:07:29 +0100

    Ejemplos

--- Tema4/Ejemplos_JQUERY/scripts/ejercicioJquery.js
+++ Tema4/Ejemplos_JQUERY/scripts/ejercicioJquery.js
@@ -1,5 +1,7 @@
 $(function(){
     $("#botonCrear").click(crearElementos);
+    $("#botonColorear").click(colorear);
+    $("#botonBorrar").click(borrarInput);
 });
 
 function crearElementos(){
@@ -14,8 +16,33 @@ function crearElementos(){
             input.attr("type","text");
             input.val(`${i}.${j}`);
             input.attr("id",`${i}_${j}`);
+            let tipo = "";
+            if(j % 2 === 0){
+               tipo = "par";     
+            }else{
+                tipo = "impar";
+            }
+            input.attr("data-tipo",tipo);
             div.append(input);
         }
         $("#elementos").append(div);
     }
 }
+
+
+function colorear(){
+    $("[data-tipo='impar']").addClass("inputImpar");
+    $("[data-tipo='par']").addClass("inputPar");
+}
+
+function borrarInput(){
+    let posi = $("#posi").val();
+    let posj = $("#posj").val();
+    let elementoABorrar = $(`[id='${posi}_${posj}']`);
+    if(elementoABorrar.length > 0){
+        elementoABorrar.remove();
+    }else{
+        alert("NO EXISTE ESE ELEMENTO");
+    }
+}
+
