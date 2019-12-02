606c7a5ad3863e23fc0e45efd9dddfea050afc90
Jorge <jorge.baron@adaits.es>
2019-12-02 21:02:32 +0100

    Formulario para votar un animal

--- Ejemplos/ejemplo_voto/usuarios.js
+++ Ejemplos/ejemplo_voto/usuarios.js
@@ -1,15 +1,35 @@
 document.addEventListener("DOMContentLoaded",function(){
-    crearSelectUsuario();
+    crearSelectUsuario("selectUsuario");
+    crearSelectUsuario("selectUsuarioVotar");
+    crearSelectAnimales("selectAnimalVotar");
+    
     let selectUsuario = document.getElementById("selectUsuario");
     selectUsuario.addEventListener("change",obtenerAnimalesVotados);
+    
+    let selectAnimalVotar = document.getElementById("selectAnimalVotar");
+    selectAnimalVotar.addEventListener("change",validarAnimal);
+    
+    let formularioVotar = document.getElementById("formularioVotar");
+    formularioVotar.addEventListener("submit",votarAnimal);
+
 })
 
-function crearSelectUsuario(){
-    let select = document.getElementById("selectUsuario"); 
+function crearSelectUsuario(idUsuario){
+    let select = document.getElementById(idUsuario); 
     for(usuario of listaUsuarios){
         let option = document.createElement("option");
         option.value = usuario.id;
         option.innerHTML = usuario.nombre;
+        select.appendChild(option);
+    }
+}
+
+function crearSelectAnimales(idAnimal){
+    let select = document.getElementById(idAnimal); 
+    for(animal of listaAnimales){
+        let option = document.createElement("option");
+        option.value = animal.id;
+        option.innerHTML = animal.nombre;
         select.appendChild(option);
     }
 }
@@ -31,5 +51,37 @@ function obtenerAnimalesVotados(event){
             liAnimal.innerHTML = animalVotado.nombre;
             ulAnimalesVotados.appendChild(liAnimal);
         }
+    }
+}
+
+function validarAnimal(){
+    let esCorrecto = true;
+    let select = document.getElementById("selectAnimalVotar");
+    let selectUsuarioVotar = document.getElementById("selectUsuarioVotar");
+    let idAnimalVotar = select.value;
+    let idUsuarioSeleccionado = selectUsuarioVotar.value;
+    let usuarioAVotar = listaUsuarios.find( persona => persona.id === parseInt(idUsuarioSeleccionado));
+    let animalAVotar = listaAnimales.find( animal => animal.id === parseInt(idAnimalVotar));
+    let haVotado = usuarioAVotar.votos.some( voto => voto.animal.id === parseInt(idAnimalVotar) );
+    if(haVotado){
+        alert(`EL USUARIO ${usuarioAVotar.nombre} YA HA VOTADO AL ANIMAL ${animalAVotar.nombre}`)
+    }
+    return esCorrecto;
+}
+
+function votarAnimal(event){
+    event.preventDefault();
+    let formulario = document.getElementById("formularioVotar");
+    let esAnimalCorrecto = validarAnimal();
+    if(esAnimalCorrecto){
+        let selectUsuario = document.getElementById("selectUsuarioVotar");
+        let selectAnimal = document.getElementById("selectAnimalVotar");
+        let idUsuarioSeleccionado = selectUsuario.value;
+        let idAnimalVotar = selectAnimal.value;
+        let usuarioAVotar = listaUsuarios.find( persona => persona.id === parseInt(idUsuarioSeleccionado));
+        let animalAVotar = listaAnimales.find( animal => animal.id === parseInt(idAnimalVotar));
+        let puntuacion = document.getElementById("puntuacion");
+        let observaciones = document.getElementById("observaciones");
+        crearVoto(usuarioAVotar,animalAVotar,puntuacion,observaciones);
     }
 }
